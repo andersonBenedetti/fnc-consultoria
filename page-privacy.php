8ef2ae6ca13d46c85575fb7f0b2042a1f21bcd6b
Anderson Benedetti <andersonjcbenedetti@gmail.com>
2025-09-30 17:14:36 -0300

    feat: add page policy

---
+++ page-privacy.php
@@ -0,0 +1,288 @@
+<?php
+// Template Name: Política de Privacidade
+?>
+
+<?php get_header(); ?>
+
+<main id="pg-policy" role="main">
+    <section class="section-hero secondary" aria-labelledby="hero-title">
+        <div class="container">
+            <h1 id="hero-title">Política de Privacidade</h1>
+        </div>
+    </section>
+
+    <section class="section-content" aria-labelledby="content-title">
+        <div class="container">
+            <div>
+                <p>A Política de Privacidade (“Política”) foi criada para demonstrar o compromisso da FNC Consultoria
+                    Corporativa EMPRESARIAL, empresa inscrita no CNPJ/ME nº 08.326.685/0001-72, (aqui referida como
+                    “FNC”, “nós” ou “nosso”), com a segurança e a privacidade das informações coletadas dos usuários,
+                    sendo estes os titulares dos seus dados pessoais (“Titular”, “Usuário” ou simplesmente “você”), bem
+                    como ajudar os titulares a tomar decisões embasadas sobre a sua privacidade.</p>
+            </div>
+
+            <div>
+                <h3>Legislação Aplicável:</h3>
+                <p>Lei nº 12.965, de 23 de abril de 2014 e Decreto nº 8.771/16 (“Marco Civil da Internet”)</p>
+                <p>Lei nº 13.709/18 (“Lei Geral de Proteção de Dados Pessoais” ou “LGPD”)</p>
+                <p>Ao utilizar nosso Site, você manifesta de maneira livre, informada, inequívoca, expressa e integral a
+                    sua
+                    concordância com esta Política.</p>
+                <p>Recomendamos a sua leitura atenta.</p>
+            </div>
+
+            <ul>
+                <li>1. PÚBLICO – ALVO</li>
+                <li>2. TRATAMENTO DE DADOS PESSOAIS</li>
+                <li>3. CONTROLADOR</li>
+                <li>4. INFORMAÇÕES QUE COLETAMOS</li>
+                <li>5. BASE LEGAL PARA O TRATAMENTO DE DADOS PESSOAIS</li>
+                <li>6. COMPARTILHAMENTO DE DADOS PESSOAIS</li>
+                <li>7. DIREITO DOS TITULARES DE DADOS PESSOAIS</li>
+                <li>8. ARMAZENAMENTO OS DADOS PESSOAIS</li>
+                <li>9. TRANSFERÊNCIA INTERNACIONAL DE DADOS PESSOAIS</li>
+                <li>10. TEMPO DE RETENÇÃO DAS INFORMAÇÕES</li>
+                <li>11. POLÍTICA DE COMUNICAÇÃO</li>
+                <li>12. CONEXÕES (LINKS) A OUTROS SITES</li>
+            </ul>
+
+            <div>
+                <h3>01. Público Alvo</h3>
+                <ul>
+                    <li>A quem se aplica?</li>
+                    <li>Aos que acessam nosso site;</li>
+                    <li>Aos que entram em contato com a FNC.</li>
+                </ul>
+            </div>
+
+            <div>
+                <h3>02. Tratamento de Dados</h3>
+                <p>Significa qualquer operação realizada com os dados pessoais. Dado pessoal: informação relacionada a
+                    pessoa
+                    natural identificada ou identificável. O dado relativo a você, Usuário do site da FNC, é um dado
+                    pessoal.</p>
+            </div>
+
+            <div>
+                <h3>3. Controlador</h3>
+                <p>Nome: FNC Consultoria</p>
+                <p>CNPJ: 08.326.685/0001-72</p>
+            </div>
+
+            <div>
+                <h3>04. Informações Coletadas</h3>
+                <p>Importante: Você não é obrigado a nos conceder suas informações, entretanto, sem conceder certas
+                    informações
+                    quando estas forem solicitadas, não lhe será permitido o acesso a algumas funcionalidades. Além dos
+                    dados
+                    que você nos informa, sempre que você interagir com o nosso Site, a FNC coletará informações de
+                    forma
+                    automática. Tais informações geralmente não são identificáveis, porém, quando combinados com outros
+                    dados
+                    gerando a capacidade de se associar a você, será tratado pela FNC como dado pessoal.</p>
+                <p>Informações coletadas quando você acessa o Site:</p>
+            </div>
+
+            <div>
+                <h3>1. Informações de navegação</h3>
+                <p>Dados coletados por meio de cookies ou device IDs, incluindo endereço de IP, datas e horários de
+                    acesso,
+                    páginas visitadas e duração da visita. Finalidade do tratamento de Cookies e tecnologias
+                    semelhantes: ativar
+                    funcionalidades especiais no site, gerar informações estatísticas, aperfeiçoamento do site e
+                    oferecer
+                    publicidade personalizada. Para mais informações, consulte a nossa Política de Cookies.</p>
+            </div>
+
+            <div>
+                <h3>2. Informações sobre o dispositivo</h3>
+                <p>Informações de hardware e software, modelo, fabricante, sistema operacional, identificadores
+                    exclusivos e
+                    dados de falha. Finalidade do tratamento de Registro de acesso: temos o dever legal de armazenar
+                    suas
+                    informações para oferecê-las as autoridades, quando for solicitado.</p>
+            </div>
+
+            <div>
+                <h3>3. formulários preenchidos no Site para contato</h3>
+                <p>Nome, email, telefone e mensagem. Finalidade do tratamento: Atender aos seus eventuais
+                    questionamentos,
+                    solicitações de informações sobre a FNC e fale conosco, assim como nossas comunicações, serviços
+                    direcionados e materiais elaborados.</p>
+            </div>
+
+            <div>
+                <h3>05. Bases Legais</h3>
+                <p>A classificação da base legal dos dados processados pela FNC ocorre mediante análise do contexto
+                    específico
+                    em que os coletamos. Os dados tratados pela FNC tendem a ser coletados somente:</p>
+                <ul>
+                    <li>Art. 7º, I da LGPD - Com seu consentimento.</li>
+                    <li>Art. 7º, II da LGPD - Para cumprimento de obrigação legal ou regulatória.</li>
+                    <li>Art. 7º, V da LGPD - Quando necessário para a execução de contrato ou de procedimentos
+                        preliminares
+                        relacionados ao contrato.</li>
+                    <li>Art. 7º, IX da LGPD - Quando o processamento dos dados pessoais está de acordo com os interesses
+                        legítimos
+                        da FNC, sempre com atenção aos seus interesses relacionados à proteção de dados ou liberdades e
+                        direitos
+                        fundamentais. Também haverá a necessidade de processamento de seus dados para proteger seus
+                        interesses
+                        ou de
+                        terceiros.</li>
+                </ul>
+            </div>
+
+            <div>
+                <h3>06. Compartilhamento</h3>
+                <p>Quando necessário para atingir as finalidades indicadas, a FNC poderá compartilhar seus dados
+                    pessoais com
+                    terceiros. Sempre que efetuado, o compartilhamento de dados será realizado dentro dos limites e
+                    propósitos
+                    do nosso negócio, em conformidade com a finalidade do tratamento dos dados pessoais e de acordo com
+                    o que
+                    autoriza a legislação aplicável. Os seus dados poderão ser compartilhados com:</p>
+                <ul>
+                    <li>Ferramenta de CRM: Envio de e-mails automáticos para os inscritos em nosso newsletter.</li>
+                    <li>Plataforma de criação e edição de sites: Hospedagem do Site.</li>
+                    <li>Servidores em nuvem: Armazenamento de informações em nuvem.</li>
+                    <li>Autoridades públicas: Resposta a denúncias, investigações, medidas judiciais e processos
+                        judiciais, caso
+                        solicitado, bem como cumprir com obrigações legais, regulatórias e fiscais.</li>
+                </ul>
+            </div>
+
+            <div>
+                <h3>07. Direitos dos Titulares</h3>
+                <ul>
+                    <li>Confirmação e acesso: Permite que você confirme a existência de tratamento de dados pessoais e,
+                        em caso</li>
+                    positivo, solicite o acesso a uma cópia destes dados.
+                    <li>Correção: Permite que você solicite a correção de dados que estejam incompletos, inexatos ou
+                        desatualizados.</li>
+                    <li>Anonimização, bloqueio ou eliminação: Permite que você solicite a anonimização de seus dados
+                        (para que
+                        eles não possam mais ser relacionados a você), o bloqueio de seus dados (suspendendo
+                        temporariamente a nossa
+                        utilização) ou eliminação de seus dados (para que os apaguemos, sem reversão).</li>
+                    <li>Portabilidade: Permite que, mediante requisição expressa, entreguemos seus dados pessoais a
+                        outro
+                        fornecedor de serviço ou produto, em formato estruturado e interoperável.</li>
+                    <li>Informações sobre o compartilhamento: Permite que você tenha saiba com quais entidades públicas
+                        e privadas
+                        nós compartilhamos os seus dados pessoais.</li>
+                    <li>Informações sobre a possibilidade de não consentir: Permite que você tenha informações claras e
+                        completas
+                        sobre as consequências de não fornecer seu consentimento, ou seja, você é livre para negar o
+                        consentimento,
+                        ainda que possamos limitar nossas entregas a você.</li>
+                    <li>Revogação do consentimento: Permite que você revogue seu consentimento dado, a qualquer tempo,
+                        sem que
+                        afete a legalidade do tratamento realizado anteriormente. Oposição: Permite que você se oponha
+                        ao tratamento
+                        de seus dados pessoais, caso você não concorde com o tratamento.</li>
+                </ul>
+            </div>
+
+            <div>
+                <p>Na forma e nos limites da legislação aplicável, o Titular poderá entrar em contato conosco para
+                    exercer seus
+                    direitos, de forma gratuita. Analisaremos o pedido e o atenderemos com a maior brevidade possível,
+                    considerando os prazos e os termos previstos na LGPD. E-mail:
+                    privacidade@fncadvocaciaempresarial.com.br</p>
+            </div>
+
+            <div>
+                <h3>08. Armazenamento</h3>
+                <p>A FNC adota medidas de segurança, técnicas e administrativas para manter em segurança todos os dados
+                    armazenados, mediante orientações internas, cláusulas de confidencialidade e com controle estrito de
+                    acesso.
+                    Especificamente com relação aos dados coletados através do nosso Site, eles são armazenados,
+                    tratados e
+                    protegidos mediante as melhores práticas de hospedagem e processamento.</p>
+                <p>IMPORTANTE: Mesmo assim, o Titular deve ter ciência que nenhum sistema de segurança na Internet é
+                    garantido
+                    contra invasões indesejadas, sendo que o compromisso da FNC se limita à adoção de medidas de
+                    proteção
+                    recomendáveis de acordo com o atual estado da técnica.</p>
+            </div>
+
+            <div>
+                <h3>09. Transferência Internacional</h3>
+                <p>A FNC realizará a transferência internacional dos seus dados para outros países ou organismos
+                    internacionais
+                    somente quando estritamente necessário para atingir a finalidade que ensejou o tratamento dos seus
+                    dados
+                    pessoais, priorizando, sempre que possível, o armazenamento em servidores localizados no Brasil.
+                    Quando seus
+                    dados pessoais forem transferidos para fora do Brasil pela FNC, adotaremos medidas apropriadas para
+                    garantir
+                    a proteção adequada de seus dados pessoais em conformidade com os requisitos das legislações
+                    aplicáveis de
+                    proteção de dados, incluindo por meio da celebração de contratos apropriados de transferência de
+                    dados com
+                    terceiros, quando necessário.</p>
+            </div>
+
+            <div>
+                <h3>10. Tempo de Retenção</h3>
+                <p>Armazenamos e mantemos suas informações:</p>
+                <ul>
+                    <li>(ii) até o término do tratamento de dados, conforme abaixo;</li>
+                    <li>(iii) pelo tempo exigido por lei;</li>
+                    <li>(iv) pelo tempo necessário para o exercício regular de direitos da FNC em processo judicial,
+                        administrativo
+                        ou arbitral.</li>
+                </ul>
+                <p>O término do tratamento de dados pessoais ocorrerá:</p>
+                <ul>
+                    <li>(i) Quando a finalidade pela qual os dados pessoais do Titular foram coletados for alcançada;
+                    </li>
+                    <li>(ii) dados pessoais coletados deixarem de ser necessários ou pertinentes ao alcance de tal
+                        finalidade;</li>
+                    <li>(iii) Quando o Titular retirar o seu consentimento ou estiver em seu direito de solicitar o
+                        término do
+                        tratamento e a exclusão de seus dados pessoais e o fizer;</li>
+                    <li>(iv) Quando houver uma determinação legal neste sentido.</li>
+                </ul>
+            </div>
+
+            <div>
+                <p>Observando o princípio da necessidade, alguns dados encaminhados podem ser excluídos da nossa base.
+                    Não
+                    temos a obrigação de armazenar os dados por tempo indeterminado e nos isentamos de qualquer
+                    responsabilidade
+                    decorrente à destruição destes dados pessoais.</p>
+            </div>
+
+            <div>
+                <h3>11. Política de Comunicação:</h3>
+                <p>Evitamos o envio de e-mails não solicitados, restringindo as comunicações com você a assuntos que
+                    sejam
+                    relevantes ou do seu específico interesse. Se você preferir não receber mais nossas informações,
+                    você pode
+                    solicitar seu descadastramento através do link de cancelamento de inscrição contido no próprio
+                    e-mail (opt
+                    out) ou diretamente para o e-mail do nosso Encarregado: privacidade@fncadvocaciaempresarial.com.br
+                </p>
+            </div>
+
+            <div>
+                <h3>12. Conexões (links) a outros Sites</h3>
+                <p>Nosso Site pode conter links ou frames de outros sites, buscando proporcionar benefícios adicionais
+                    aos
+                    conteúdos e serviços oferecidos ao Usuário. Na medida em que os sites vinculados não fazem parte do
+                    nosso
+                    Site, nós não controlamos ou endossamos esses sites e seus conteúdos e suas práticas de privacidade;
+                    não
+                    temos acesso aos dados pessoais coletados e tratados por eles e às suas políticas de privacidade,
+                    cookies e
+                    termos de uso. Dessa forma, recomendamos a sua leitura atenta a estes documentos de sites terceiros.
+                    Ao
+                    utilizar os nossos serviços, você concorda e aceita os termos aqui dispostos.</p>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php get_footer(); ?>
